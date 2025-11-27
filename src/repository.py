879a8d46fe9a164abd333704e83d77de1c7d9bc4
esillileu <esillileu@gmail.com>
2025-11-27 11:36:59 +0900

    feat: Update repository to handle new schema relationships

--- src/repository.py
+++ src/repository.py
@@ -2,56 +2,88 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 from src.models import HanjaInfo, UsageExample
 
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+from src.models import HanjaInfo, UsageExample, Document, DocumentHanja, DocumentWord, HanjaReading
+
 class HanjaRepository:
     def __init__(self):
         pass
 
-    def add_hanja_info(self, session, char: str, sound: str, meaning: str = None, radical: str = None, strokes: int = None) -> HanjaInfo:
-        try:
-            hanja = session.query(HanjaInfo).filter_by(char=char).first()
-            if hanja:
-                hanja.sound = sound
-                hanja.meaning = meaning
-                hanja.radical = radical
-                hanja.strokes = strokes
-            else:
-                hanja = HanjaInfo(char=char, sound=sound, meaning=meaning, radical=radical, strokes=strokes)
-                session.add(hanja)
-            session.flush() # Flush to assign ID if new, but don't commit yet
-            return hanja
-        except IntegrityError:
-            session.rollback()
-            existing_hanja = session.query(HanjaInfo).filter_by(char=char).first()
-            return existing_hanja
-        except Exception as e:
-            session.rollback()
-            raise e
+    def get_document_by_hash(self, session, file_hash: str) -> Document:
+        return session.query(Document).filter_by(file_hash=file_hash).first()
 
-    def get_hanja_info(self, session, char: str) -> HanjaInfo:
-        return session.query(HanjaInfo).filter_by(char=char).first()
+    def create_document(self, session, filename: str, file_hash: str) -> Document:
+        doc = Document(filename=filename, file_hash=file_hash)
+        session.add(doc)
+        session.flush()
+        return doc
+
+    def add_hanja_info(self, session, char: str, sound: str, meaning: str = None, radical: str = None, strokes: int = None, readings: list = None) -> HanjaInfo:
+        hanja = session.query(HanjaInfo).filter_by(char=char).first()
+        if not hanja:
+            hanja = HanjaInfo(char=char, radical=radical, strokes=strokes)
+            session.add(hanja)
+            session.flush()
+        else:
+            # Update info if missing
+            if radical and not hanja.radical: hanja.radical = radical
+            if strokes and not hanja.strokes: hanja.strokes = strokes
+        
+        # Process readings list
+        if readings:
+            for reading_data in readings:
+                r_sound = reading_data.get('sound')
+                r_meaning = reading_data.get('meaning')
+                if r_sound:
+                    reading = session.query(HanjaReading).filter_by(hanja_id=hanja.id, sound=r_sound, meaning=r_meaning).first()
+                    if not reading:
+                        reading = HanjaReading(hanja_id=hanja.id, sound=r_sound, meaning=r_meaning)
+                        session.add(reading)
+        else:
+            # Fallback for single sound/meaning provided as args (legacy support)
+            reading = session.query(HanjaReading).filter_by(hanja_id=hanja.id, sound=sound).first()
+            if not reading:
+                reading = HanjaReading(hanja_id=hanja.id, sound=sound, meaning=meaning)
+                session.add(reading)
+        
+        return hanja
+
+    def update_document_hanja_frequency(self, session, document_id: int, hanja_char: str) -> DocumentHanja:
+        hanja = session.query(HanjaInfo).filter_by(char=hanja_char).first()
+        if not hanja:
+            return None # Should act after add_hanja_info
+            
+        doc_hanja = session.query(DocumentHanja).filter_by(document_id=document_id, hanja_id=hanja.id).first()
+        if doc_hanja:
+            doc_hanja.frequency += 1
+        else:
+            doc_hanja = DocumentHanja(document_id=document_id, hanja_id=hanja.id, frequency=1)
+            session.add(doc_hanja)
+        return doc_hanja
 
     def add_usage_example(self, session, word: str, sound: str = None) -> UsageExample:
-        try:
-            example = session.query(UsageExample).filter_by(word=word).first()
-            if example:
-                example.frequency += 1
-                if sound and not example.sound:
-                    example.sound = sound
-            else:
-                example = UsageExample(word=word, sound=sound, frequency=1)
-                session.add(example)
-            session.flush() # Flush to assign ID if new, but don't commit yet
-            return example
-        except IntegrityError:
-            session.rollback()
-            existing_example = session.query(UsageExample).filter_by(word=word).first()
-            return existing_example
-        except Exception as e:
-            session.rollback()
-            raise e
+        example = session.query(UsageExample).filter_by(word=word).first()
+        if not example:
+            example = UsageExample(word=word, sound=sound)
+            session.add(example)
+            session.flush()
+        elif sound and not example.sound:
+            example.sound = sound
+        return example
 
-    def get_usage_example(self, session, word: str) -> UsageExample:
-        return session.query(UsageExample).filter_by(word=word).first()
+    def update_document_word_frequency(self, session, document_id: int, word_str: str) -> DocumentWord:
+        word = session.query(UsageExample).filter_by(word=word_str).first()
+        if not word:
+            return None
+            
+        doc_word = session.query(DocumentWord).filter_by(document_id=document_id, word_id=word.id).first()
+        if doc_word:
+            doc_word.frequency += 1
+        else:
+            doc_word = DocumentWord(document_id=document_id, word_id=word.id, frequency=1)
+            session.add(doc_word)
+        return doc_word
 
     def get_all_hanja_info(self, session):
         return session.query(HanjaInfo).all()
